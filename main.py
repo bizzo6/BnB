deaa21cb01311189055efbb19f4706ccc37312b0
bizzo6 <talnati@gmail.com>
2015-09-30 22:46:20 +0300

    First commit with PyDev Project and main

---
+++ main.py
@@ -0,0 +1,19 @@
+# -*- coding: UTF-8 -*-
+'''
+Created on 30 בספט׳ 2015
+
+@author: Nati
+'''
+
+def main():
+    print "Banana Nafal Blat - (BnB) v0.1"
+    
+    return
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
